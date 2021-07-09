b2c88dd045c9d992cdf6e932e6839834bfe1556f
Obskurcyte <hadrien.jaubert@centrale-marseille>
2021-07-09 10:01:07 +0200

    fixes

--- components/SideBar.js
+++ components/SideBar.js
@@ -134,94 +134,90 @@ const SideBar = props => {
 
   return (
      <div className={isActive ? "container-sideBarActive display" : "container-sideBar display"}>
-      <img src={'/bicornLogo2.png'} alt="" className={isActive ? 'active' : "bicornSideBarLogo"}/>
-      <Link href='/'>
-      <div className={isActive ? "linkContainerActive" : props.classNameHome}>
-        <img src={'/icone2.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
-          <p className={isActive ? 'active' : ''}>Home</p>
-      </div>
-      </Link>
-      <div className={isActive ? "linkContainerActive" : "linkContainer"}>
-        <img src={'/icone3.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
-        <p className={isActive ? 'active' : ''}>Trade</p>
-      </div>
-      <Link
-        href={'/hives'}
-      >
-      <div className={isActive ? "linkContainerActive" : props.classNameHives}>
-        <img src={'/iconeAbeille.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
-
-          <p className={isActive ? 'active' : ''}>Hives</p>
-
-      </div>
-      </Link>
-      <Link href={'/stakes'}>
-        <div className={isActive ? "linkContainerActive" : props.classNameStackes}>
-          <img src={'/icone5.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
-          <p className={isActive ? 'active' : ''}>Stakes</p>
+        <img src={'/bicornLogo2.png'} alt="" className={isActive ? 'active' : "bicornSideBarLogo"}/>
+        <Link href='/'>
+        <div className={isActive ? "linkContainerActive" : props.classNameHome}>
+          <img src={'/icone2.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
+            <p className={isActive ? 'active' : ''}>Home</p>
         </div>
-      </Link>
-      <Link href={'/nap'}>
-      <div className={isActive ? "linkContainerActive" : props.classNameNap}>
-        <img src={'/icone6.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
-          <p className={isActive ? 'active' : ''}>Nap</p>
-      </div>
-      </Link>
-      <Link href={'/lottery'}>
-        <div className={isActive ? "linkContainerActive" : props.classNameLottery}>
-          <img src={'/icone10.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
+        </Link>
+        <div className={isActive ? "linkContainerActive" : "linkContainer"}>
+          <img src={'/icone3.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
+          <p className={isActive ? 'active' : ''}>Trade</p>
+        </div>
+        <Link
+          href={'/hives'}
+        >
+        <div className={isActive ? "linkContainerActive" : props.classNameHives}>
+          <img src={'/iconeAbeille.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
 
-            <p className={isActive ? 'active' : ''}>Lottery</p>
+            <p className={isActive ? 'active' : ''}>Hives</p>
 
         </div>
-      </Link>
-      <div className={isActive ? "linkContainerActive" : "linkContainer"}>
-        <img src={'/icone8.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
-        <p className={isActive ? 'active' : ''}>N-FTs</p>
-      </div>
-      <div className={isActive ? "linkContainerActive" : "linkContainer"}>
-        <img src={'/icone4.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
-        <p className={isActive ? 'active' : ''}>INO</p>
-      </div>
-      <div className={isActive ? "linkContainerActive" : "linkContainer"}>
-        <img src={'/icone9.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
-        <p className={isActive ? 'active' : ''}>Team & Profile</p>
-      </div>
-      <div className={isActive ? "linkContainerActive" : "linkContainer"}>
-        <img src={'/icone11.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
-        <p className={isActive ? 'active' : ''}>Features</p>
-      </div>
-      <div className={isActive ? "linkContainerActive" : "linkContainer"}>
-        <img src={'/icone7.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
-        <p className={isActive ? 'active' : ''}>Civil Code</p>
-      </div>
-      <div className={isActive ? "linkContainerActive" : "linkContainer"}>
-        <img src={'/icone13.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
-        <p className={isActive ? 'active' : ''}>Conquest</p>
-      </div>
+        </Link>
+        <Link href={'/stakes'}>
+          <div className={isActive ? "linkContainerActive" : props.classNameStackes}>
+            <img src={'/icone5.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
+            <p className={isActive ? 'active' : ''}>Stakes</p>
+          </div>
+        </Link>
+        <Link href={'/nap'}>
+        <div className={isActive ? "linkContainerActive" : props.classNameNap}>
+          <img src={'/icone6.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
+            <p className={isActive ? 'active' : ''}>Nap</p>
+        </div>
+        </Link>
+        <Link href={'/lottery'}>
+          <div className={isActive ? "linkContainerActive" : props.classNameLottery}>
+            <img src={'/icone10.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
 
-      <div className="links">
-        <div className={isActive ? "flex-column" : "miniTotal flex"}>
+              <p className={isActive ? 'active' : ''}>Lottery</p>
+
+          </div>
+        </Link>
+        <div className={isActive ? "linkContainerActive" : "linkContainer"}>
+          <img src={'/icone8.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
+          <p className={isActive ? 'active' : ''}>N-FTs</p>
+        </div>
+        <div className={isActive ? "linkContainerActive" : "linkContainer"}>
+          <img src={'/icone4.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
+          <p className={isActive ? 'active' : ''}>INO</p>
+        </div>
+        <div className={isActive ? "linkContainerActive" : "linkContainer"}>
+          <img src={'/icone9.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
+          <p className={isActive ? 'active' : ''}>Team & Profile</p>
+        </div>
+        <div className={isActive ? "linkContainerActive" : "linkContainer"}>
+          <img src={'/icone11.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
+          <p className={isActive ? 'active' : ''}>Features</p>
+        </div>
+        <div className={isActive ? "linkContainerActive" : "linkContainer"}>
+          <img src={'/icone7.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
+          <p className={isActive ? 'active' : ''}>Civil Code</p>
+        </div>
+        <div className={isActive ? "linkContainerActive" : "linkContainer"}>
+          <img src={'/icone13.png'} alt="" className={isActive ? "imgActive" : "imgSideBar"}/>
+          <p className={isActive ? 'active' : ''}>Conquest</p>
+        </div>
+
+        <div className="links">
           <div className={isActive ? "flex-column" : "miniTotal flex"}>
-            <img src={'/LogoBicorn.png'} alt="" className={isActive ? "logoImageActive" : "logoImage"}/>
-            <h5 className={isActive ? "priceMiniActive" : "priceMini"}>12, 984€</h5>
+            <div className={isActive ? "flex-column" : "miniTotal flex"}>
+              <img src={'/LogoBicorn.png'} alt="" className={isActive ? "logoImageActive" : "logoImage"}/>
+              <h5 className={isActive ? "priceMiniActive" : "priceMini"}>12, 984€</h5>
+            </div>
           </div>
-          <div className={isActive ? "flex-column" : "flex"}>
-            <img src={'/Mapicon.png'} alt="" className={isActive ? "logoImageActive" : "logoImage"}/>
-            <p className={isActive ? 'langActive' : "lang"} >EN</p>
+
+            <div className={isActive ? "flex-column" : "miniTotal flex"}>
+              <img src={'/githubIcon.png'} alt="" className={isActive ? "img-linksActive" : "img-links"}/>
+              <img src={'/Twittericon.png'} alt="" className={isActive ? "img-linksActive" : "img-links"}/>
+              <img src={'/Mailicon.png'} alt="" className={isActive ? "img-linksActive" : "img-links"}/>
+            </div>
           </div>
+
+        <div className="black">
+
         </div>
-
-          <div className={isActive ? "flex-column" : "miniTotal flex"}>
-            <img src={'/githubIcon.png'} alt="" className={isActive ? "img-linksActive" : "img-links"}/>
-            <img src={'/Twittericon.png'} alt="" className={isActive ? "img-linksActive" : "img-links"}/>
-            <img src={'/Mailicon.png'} alt="" className={isActive ? "img-linksActive" : "img-links"}/>
-          </div>
-        </div>
-
-      <div className="black">
-
-      </div>
       </div>
 
 
